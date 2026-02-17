86b5043fcc2c8d22826288ff666a72b28a2d8d25
vahidi <r.vahidi1023@yahoo.com>
2026-02-18 00:01:06 +0330

    init

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UrlController extends Controller
+{
+
+    public function __construct(private readonly UrlService $urlService)
+    {
+    }
+
+    /**
+     * 📄 لیست کاربران
+     */
+    public function index(Request $request, int $perPage = 20)
+    {
+        $users = $this->urlService->getAll($request, $perPage);
+        $response = UrlResource::collection($users)
+            ->response()
+            ->getData(true);
+
+        unset($response['links']);
+        return $this->responseSuccess('get data successfully', $response);
+    }
+    public function show(int $id, Request $request)
+    {
+        $user = $this->urlService->find($id, $request);
+        return $this->responseSuccess('get data successfully', new UserResource($user));
+    }
+
+    public function store(CreateUrlRequest $request, int $id)
+    {
+        $user = $this->urlService->create($id, $request->validated());
+
+        $this->user_activity(
+            'update Profile',
+            'update Profile Successfully'
+        );
+        return $this->responseCreated('item successfully created', new UrlResource($user));
+    }
+    public function update(UpdateUrlRequest $request, int $id)
+    {
+        $user = $this->urlService->update($id, $request->validated());
+
+        $this->user_activity(
+            'update Profile',
+            'update Profile Successfully'
+        );
+        return $this->responseCreated('item successfully updated', new UrlResource($user));
+    }
+    public function delete(int $id)
+    {
+        $user = $this->urlService->delete($id);
+
+        $this->user_activity(
+            'update Profile',
+            'update Profile Successfully'
+        );
+        return $this->responseCreated(__('message.item successfully created'), new UrlResource($user));
+    }
+
+
+
+
+}
---
+++ app/Models/url.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class url extends Model
+{
+    use HasFactory;
+    //STATUS
+    const STATUS_NOT_ACTIVE = 'nonactive';
+    const STATUS_ACTIVE = 'active';
+
+    protected $table = 'urls';
+    /**
+     * Mass-assignable attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        "original_url",
+        "short_code",
+        "status",
+        "click",
+    ];
+
+    /**
+     * Returns user statuses list
+     * @return array
+     */
+    public static function statuses()
+    {
+        return [
+            self::STATUS_NOT_ACTIVE => ['title' => "فعال"],
+            self::STATUS_ACTIVE => ['title' => "غیر فعال"],
+        ];
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+
+    ];
+
+
+
+}
